532c63f0b4a21b441a2a092b70d935ace7cfbff0
Ericmohn <eric_mohn@hotmail.com>
2022-11-29 17:22:03 -0300

    first commit

---
+++ src/data.js
@@ -0,0 +1,40 @@
+const reviews = [
+  {
+    id: 1,
+    name: 'Squidward Q. Tentacles',
+    job: 'Cashier',
+    image:
+      'https://www.nicepng.com/png/detail/179-1795474_handsome-squidward-png-handsome-squidward.png',
+    text:
+      "Clarinet player",
+  },
+  {
+    id: 2,
+    name: 'Patrick Star',
+    job: 'Telemarketer',
+    image:
+      'https://i.redd.it/w8cd51e978g51.jpg',
+    text:
+      'This is Patrick!',
+  },
+  {
+    id: 3,
+    name: 'SpongeBob Squarepants',
+    job: 'Cook',
+    image:
+      'https://imgix.ranker.com/user_node_img/50095/1001880179/original/a-sick-spongebob-photo-u1?auto=format&q=60&fit=crop&fm=pjpg&dpr=2&w=375',
+    text:
+      "I'm ready!",
+  },
+  {
+    id: 4,
+    name: 'Eugene Mr. Krabs',
+    job: 'The Boss',
+    image:
+      'https://imgix.ranker.com/user_node_img/50095/1001880200/original/mr-krabs-could-use-a-break-photo-u1?auto=format&q=60&fit=crop&fm=pjpg&dpr=2&w=375',
+    text:
+      'I love money, argharghargh',
+  },
+];
+
+export default reviews;
